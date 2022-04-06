1f2515f0b5de42ace382d2ee9e58ea2605aade71
PabloDavidAR <pablo.arandarod@gmail.com>
2022-04-06 12:58:55 +0200

    Changed control functions and communication structure

--- src/real_robot_interface_py/real_robot_interface_py/robot_interface.py
+++ src/real_robot_interface_py/real_robot_interface_py/robot_interface.py
@@ -3,66 +3,64 @@ from rtde_receive import RTDEReceiveInterface
 from rtde_io import RTDEIOInterface
 import numpy as np
 import sys
+import datetime
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Bool, String
+from custom_msgs_srvs.srv import SensorCall
 
 dataArray = Float32MultiArray
-
-def torqueToSpeed(torque: np.array):
-    pass
 
 class robotInterface(Node):
     ''' class robotInterface: Communicate with the real robot sending the commands and reading the different sensor values.'''
     def __init__(self):
-        super().__init__("UR_interface")
+        super().__init__("robot_interface")
 
         # Declare parameters
         self.declare_parameter("ip")
         self.declare_parameter("dt")
 
-        # Declare 
+        # Read parameters from command line
         self.ip = self.get_parameter("ip").get_parameter_value().string_value
         if self.ip == '':
             print(f"[ERROR] Incorrect IP input: ({self.ip})")
             sys.exit()
+        
         self.dt = self.get_parameter("dt").get_parameter_value().double_value
         if self.dt == 0:
             print(f"[ERROR] Incorrect sampling time input: ({self.dt})")
             sys.exit()
-        print(f"IP is: {self.ip}")
-        print(f"dt is: {self.dt}")
 
-        # Publishers
-        self.action_subscriber = self.create_subscription(dataArray, 'action', self.action_callback, 10)
 
-        # Sensor reading and publishing
-        self.sensor_timer = self.create_timer(self.dt, self.sensor_callback)
-        self.input_publisher = self.create_publisher(dataArray, 'robot_input',10)
+        # Subscribers
+        self.controller_subscriber = self.create_subscription(dataArray, '/ur/output_controller', self.position_callback, 10)
+        self.gui_subscriber = self.create_subscription(dataArray, '/gui/position', self.gui_callback, 10)
+
+        # Services
+        self.sensor_request = self.create_service(SensorCall, "/ur/sensor", self.sensor_callback)
 
         # Communication variables with UR
         self.control = RTDEControlInterface(self.ip)
         self.receive = RTDEReceiveInterface(self.ip)
-    
-    def action_callback(self, msg: dataArray):
-        torque = np.reshape(np.array(msg.data), (6,1))
-        speed = torqueToSpeed(torque)
-        self.control.speedJ(speed)
-        pass
 
-    def sensor_callback(self):
-        msg = dataArray(self.receive.getActualQ() + self.receive.getActualQd() + self.receive.getJointTorques())
-        self.input_publisher(msg)
+    def sensor_callback(self, request, response):
+        data_list = self.receive.getActualQ() + self.receive.getActualQd() + self.control.getJointTorques()
+        response.data = data_list
+        return response
+
+    def position_callback(self, msg):
+        self.control.moveL(msg.data[0:6])
+
+    def gui_callback(self, msg):
+        self.control.moveJ(msg.data[0:6])
 
 def main(args=None):
     rclpy.init(args=args)
-
     robot = robotInterface()
     rclpy.spin(robot)
     robot.destroy_node()
     rclpy.shutdown()
 
 if __name__=="__main__":
-    
     main()
