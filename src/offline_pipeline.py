7177bb81cb887502c5b90bfac9c2f05d4da52164
manuotel <manuotel@yahoo.com>
2022-04-15 22:56:36 +0200

    untested gmr

--- src/offline_pipeline.py
+++ src/offline_pipeline.py
@@ -1,13 +1,15 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-sys.path.append("./src/dataHandling")
+sys.path.append(os.path.normpath(os.getcwd() + os.sep + os.pardir) + "./src/dataHandling")
+
 import dataHandling.data_utilities as du
 
-task_dir = "resources/training_data/task_1/"
+task_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "/resources/training_data/task_1/"
 
-list_datasets, dataset = du.combine(task_dir=task_dir, n = 5)
+list_datasets, dataset = du.combine(task_dir=task_dir, n=10)
 
-fig_traj, axs_traj = du.plotTrajectory(list_datasets[0][:,:6])
+fig_traj, axs_traj = du.plotTrajectory(list_datasets[0][:, :6])
 plt.show()
